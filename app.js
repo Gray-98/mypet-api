092ffed4a0fbbbf3f7c0d48b63ebd39276d532bc
Gray <1292981864@qq.com>
2024-04-23 21:27:09 +0800

    Update port message

--- app.js
+++ app.js
@@ -3,14 +3,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const PORT = 8300
 
 app.use(bodyParser.json());
 require('./routes')(app);
 
-app.listen(8300, err => {
+app.listen(PORT, err => {
 	if (err) {
 		console.log(err);
 	}
 
-	console.log('Server is running on port 8080......');
+	console.log(`Server is running on port ${PORT}......`);
 });
